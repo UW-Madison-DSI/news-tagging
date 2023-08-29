5f3ff53fddb79561a3cab60e8ad8942be4d9e9ab
Jason Lo <JasonLo@users.noreply.github.com>
2023-08-29 12:07:20 -0500

    implement tag filter (#2)
    Addressing #1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import logging
 import streamlit as st
 from cachetools import TTLCache, cached
 
-from data_loaded import Post, download_posts, load_posts
+from data_loader import Post, download_posts, load_posts, get_all_tags, filter_posts
 from gpt import tag
 
 
@@ -28,7 +28,7 @@ def get_posts() -> list[Post]:
     return existing_posts
 
 
-st.set_page_config(page_title="UW News tagging", page_icon="📰")
+st.set_page_config(page_title="UW News tagging", page_icon="📰", layout="wide")
 st.title("UW News tagging (prototype)")
 
 """
@@ -41,21 +41,35 @@ derived from both the title and the complete content of each article.
 """
 
 st.header("📰 UW News articles")
+st.session_state.filter_selection = None
 
 posts = get_posts()
+tags = get_all_tags(posts)
 
-for post in posts:
+with st.expander("🏷️ Filter articles by Tags"):
+    # Show tags in a grid
+    N_TAGS_PER_ROW = 5
+    for i in range(0, len(tags), N_TAGS_PER_ROW):
+        row_tags = tags[i : i + N_TAGS_PER_ROW]
+
+        # Put tags button in a row
+        for tag, col in zip(row_tags, st.columns(N_TAGS_PER_ROW)):
+            if col.button(tag, use_container_width=True):
+                st.session_state.filter_selection = tag
+
+# Show posts
+for post in filter_posts(posts, st.session_state.filter_selection):
     st.subheader(post.title)
     st.write(f"Summary: {post.summary}")
 
     left_col, right_col = st.columns(2, gap="medium")
 
     left_col.subheader("Original tags")
-    left_col.write(', '.join(post.tags_original))
+    left_col.write(", ".join(post.tags_original))
 
     right_col.subheader("GPT-4 tags")
-    right_col.write(', '.join(post.tags_gpt))
+    right_col.write(", ".join(post.tags_gpt))
 
-    with st.expander("Show more details"):
+    with st.expander("🔎 Show more details"):
         st.write("Raw post object in json:")
         st.json(post.model_dump_json())
