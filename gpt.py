b06c2a44979c0d705a8d552a311bbe1c8c3716d5
Jason Lo <lcmjlo@gmail.com>
2023-08-31 15:46:24 +0000

    prototype user defined available tags

--- gpt.py
+++ gpt.py
@@ -1,7 +1,9 @@
 import ast
+import asyncio
 import os
 
 import openai
+from aiohttp import ClientSession
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,7 +11,7 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def tag(title: str, content: str):
+def tag(title: str, content: str) -> list[str]:
     """Tag news into one or more category / tags."""
 
     system_message = {
@@ -26,5 +28,41 @@ def tag(title: str, content: str):
         model="gpt-4", messages=[system_message, user_message], temperature=0.9
     )
 
-    assert len(response["choices"]) == 1
     return ast.literal_eval(response["choices"][0]["message"]["content"])
+
+
+async def user_tag(title: str, content: str, target_tags: list) -> list[str]:
+    """Tag news into one or more targeted category / tags."""
+
+    system_message = {
+        "role": "system",
+        "content": f"You are a journalist who can accurately categorized news into one or more categories / tags. \
+            Pick one or more categories / tags from the following list: {target_tags}. \
+            You organize your output like a python list, similar to this ['tag1', 'tag2']",
+    }
+
+    user_message = {
+        "role": "user",
+        "content": f"Please tag the following news into one or more categories: '{title} {content}'",
+    }
+
+    response = await openai.ChatCompletion.acreate(
+        model="gpt-4", messages=[system_message, user_message], temperature=0.9
+    )
+
+    return ast.literal_eval(response["choices"][0]["message"]["content"])
+
+
+async def batch_user_tag(
+    titles: list[str], contents: list[str], target_tags: list
+) -> list[list[str]]:
+    """Tag news in batch mode in targeted categories."""
+
+    openai.aiosession.set(ClientSession())
+    async_responses = [
+        user_tag(title, content, target_tags)
+        for title, content in zip(titles, contents)
+    ]
+    user_tag_results = await asyncio.gather(*async_responses)
+    await openai.aiosession.get().close()
+    return user_tag_results
