491b9ba4fa610437eae5f065f62c94e996f503c8
Jason Lo <lcmjlo@gmail.com>
2023-09-05 12:14:22 -0500

    fix deployment script

--- scripts/deplot_wisc.sh
+++ scripts/deplot_wisc.sh
@@ -1,3 +1,3 @@
 pip install -U rsconnect-python
-rsconnect add --server https://connect.doit.wisc.edu --name wise --api-key $POSIT_API_KEY
+rsconnect add -i --server https://connect.doit.wisc.edu --name wise --api-key $POSIT_API_KEY
 rsconnect deploy streamlit -n wisc --entrypoint api:app .
