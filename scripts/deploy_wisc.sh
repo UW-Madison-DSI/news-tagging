5bc3cfb2c7491c5ddc984a71453737dbace6e474
Jason Lo <lcmjlo@gmail.com>
2023-12-20 22:39:32 +0000

    fix typo

--- scripts/deploy_wisc.sh
+++ scripts/deploy_wisc.sh
@@ -1,3 +1,3 @@
 pip install -U rsconnect-python
-rsconnect add -i --server https://connect.doit.wisc.edu --name wise --api-key $POSIT_API_KEY
-rsconnect deploy streamlit -n wisc --entrypoint api:app .
+rsconnect add -i --server https://connect.doit.wisc.edu --name wisc --api-key $POSIT_API_KEY
+rsconnect deploy streamlit -n wisc --entrypoint app.py .
